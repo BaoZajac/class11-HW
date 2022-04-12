6ba0bd6582a041de52462909a4b2f36c845ef73a
Basia <bao.zajac@gmail.com>
2022-04-12 15:55:25 +0200

    dodanie sprawdzania poprawnosci wprowadzenia pliku wejsciowego, spr. rozszerzenia pliku wejsciow i wyjsciow, obsluzenie plikow .csv

--- reader.py
+++ reader.py
@@ -96,9 +96,37 @@ class JSONFile(OpenChangeSaveFile):
         self.open_file()
 
 
+# ------------------------------------------------------------------------------------
 
 file_old = sys.argv[1]
 file_new = sys.argv[2]
+
+if not os.path.exists(file_old) or os.path.isdir(file_old):
+    print("Wprowadzono błędną nazwę pliku wejściowego. Dostępne pliki to:")
+    os.system("ls")
+
+# available_ext = {".csv": "CSVFile", ".json": "JSONFile", ".pkl": "PickleFile"}
+available_ext = [".csv", ".json", ".pkl"]
+
+ext_old = os.path.splitext(file_old)[-1]
+ext_new = os.path.splitext(file_new)[-1]
+if ext_old not in available_ext or ext_new not in available_ext:
+    print("Nieobsługiwany format pliku")
+else:
+    if ext_old == ".csv":
+        CSVFile(file_old, file_new).print_data()        # +
+    elif ext_old == ".json":
+        ...
+    elif ext_old == ".pkl":
+        ...
+    if ext_new == ".csv":
+        CSVFile(file_old, file_new).save_file()         # +
+    elif ext_new == ".json":
+        ...
+    elif ext_new == ".pkl":
+        print("wyjście to pickle")
+
+
 
 # # OpenChangeSaveFile(file_old).check_file_ext_old()
 # CSVFile(file_old, file_new).check_file_ext_old()    +
@@ -113,7 +141,5 @@ file_new = sys.argv[2]
 # PickleFile(file_old, file_new).save_file()        -
 
 
-JSONFile(file_old, file_new).open_file()
+# JSONFile(file_old, file_new).open_file()
 
-
-# if not os.path.exists([sys.argv[1]])
