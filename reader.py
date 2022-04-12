99cb0aa95a7db2261e4ce21bfe15a5eaf89b2d6f
Basia <bao.zajac@gmail.com>
2022-04-12 14:48:19 +0200

    naprawienie niedziałania w gałęzi druga_galaz

--- reader.py
+++ reader.py
@@ -6,35 +6,37 @@ import pickle
 
 
 class OpenChangeSaveFile:
-    def __init__(self, file_path_old):      #, file_path_new):
+    def __init__(self, file_path_old, file_path_new):
         self.file_path_old = file_path_old
-        # self.file_path_new = file_path_new
+        self.file_path_new = file_path_new
         self.lista = []
 
     def check_file_ext_old(self):
-        ext = os.path.splitext(self.file_path_old)[-1]      # TODO: 4) jak skierować do odpowiedniej klasy po spr.rozszerzenia?
-        return ext
+        ext_old = os.path.splitext(self.file_path_old)[-1]      # TODO: 4) jak skierować do odpowiedniej klasy po spr.rozszerzenia?
+        print("Old extension: ", ext_old)
+        return ext_old
     
-    # def check_file_ext_new(self):
-    #     ext = os.path.splitext(self.file_path_new)[-1]
-    #     return ext
+    def check_file_ext_new(self):
+        ext_new = os.path.splitext(self.file_path_new)[-1]
+        print("New extension: ", ext_new)
+        return ext_new
 
     def open_file(self):
         pass
-        # print(self.lista)       # TODO: 1) dlaczego to nie działa dla open_file w CSVFile?
+        # print("To co jest wczytywane: ", self.lista)
 
-    # def switch_list(self):    # modyfikacja elementu listy na podstawie danych z std
-    #     self.open_file()
-    #     for idx in sys.argv[3:]:
-    #         y, x, wartosc = idx.split(",")
-    #         self.lista[int(y)][int(x)] = wartosc
-    #     # print(self.lista)
-    #     return self.lista
+    def switch_list(self):    # modyfikacja elementu listy na podstawie danych z std
+        self.open_file()
+        for idx in sys.argv[3:]:
+            y, x, wartosc = idx.split(",")
+            self.lista[int(y)][int(x)] = wartosc
+        # print(self.lista)
+        return self.lista
 
-    # def print_data(self):       # TODO: 3) czy ma wydrukować po prostu listę?
-    #     self.switch_list()
-    #     print(self.lista)
-    #
+    def print_data(self):       # TODO: 3) czy ma wydrukować po prostu listę?
+        self.switch_list()
+        print(self.lista)
+
     # def save_file(self):
     #     self.switch_list()
         
@@ -45,48 +47,73 @@ class CSVFile(OpenChangeSaveFile):
             reader = csv.reader(f)
             for linia in reader:
                 self.lista.append(linia)
+        # super().open_file()
         # print(self.lista)
         return self.lista
 
-    # def save_file(self):
-    #     with open(self.file_path_new, "w") as f:
-    #         writer = csv.writer(f)
-    #         writer.writerows(self.lista)
+    def save_file(self):
+        self.switch_list()
+        # print(self.lista)
+        with open(self.file_path_new, "w", newline="") as f:
+            writer = csv.writer(f)
+            for linia in self.lista:
+                writer.writerow(linia)
+            # writer.writerows(self.lista)
 
 
-# class PickleFile(OpenChangeSaveFile):
+class PickleFile(OpenChangeSaveFile):
 #     def open_file(self):                # TODO: sprawdzić działanie, gdy będę mieć plik typu pickle
 #         with open(self.file_path_old, "rb") as f:
 #             reader = pickle.load(f)
 #             print(reader)
 
-
-# class JSONFile(OpenChangeSaveFile):
-#     def open_file(self):                # TODO : 2) jak zmienić plik json na listę list, gdy nie znamy zawartości danego pliku?
-#         # lista = []
-#         with open(self.file_path_old, "r") as f:
-#             reader = json.load(f)
-#             print(reader)
-#             # print(reader)
-#             for k in reader:
-#                 # self.lista.append(linia)
-#                 print(k)
-#                 # print(el2)
-#         # print(self.lista)
+    def save_file(self):
+        self.switch_list()
+        # print(self.lista)
+        with open(self.file_path_new, "wb") as f:
+            pickle.dump(self.lista, f)
+            # writer = csv.writer(f)
+            # for linia in self.lista:
+            #     writer.writerow(linia)
 
 
-src_file = sys.argv[1]
-# file_new = sys.argv[2]
+class JSONFile(OpenChangeSaveFile):
+    def open_file(self):                # TODO : 2) jak zmienić plik json na listę list, gdy nie znamy zawartości danego pliku?
+        # lista = []
+        with open(self.file_path_old, "r") as f:
+            reader = json.load(f)
+            print(reader)
+            # for k, v in reader.items():
+            #     self.lista.append([k, v])
+            # print(reader)
+            # for k in reader:
+            #     # self.lista.append(linia)
+            #     print(k)
+                # print(el2)
+        # print(self.lista)
 
-# # OpenChangeSaveFile(src_file).check_file_ext_old()
-# CSVFile(src_file).check_file_ext_old()
+    def switch_list(self):    # modyfikacja elementu słownika na podstawie danych z std
+        self.open_file()
 
-CSVFile(src_file).open_file()
-# print(lista)
-# JSONFile(src_file).open_file()
-# CSVFile(src_file).switch_list()
 
-# CSVFile(src_file).print_data()
-# CSVFile(src_file).save_file()
+
+file_old = sys.argv[1]
+file_new = sys.argv[2]
+
+# # OpenChangeSaveFile(file_old).check_file_ext_old()
+# CSVFile(file_old, file_new).check_file_ext_old()    +
+# CSVFile(file_old, file_new).check_file_ext_new()    +
+#
+# # CSVFile(file_old, file_new).open_file()
+# # CSVFile(file_old, file_new).switch_list()
+#
+# CSVFile(file_old, file_new).print_data()        +
+# CSVFile(file_old, file_new).save_file()         +
+
+# PickleFile(file_old, file_new).save_file()        -
+
+
+JSONFile(file_old, file_new).open_file()
+
 
 # if not os.path.exists([sys.argv[1]])
